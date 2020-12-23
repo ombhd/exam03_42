07e47f0b3d5a9e13a75dcbe7b4e3ea65a212a2fc
Omar Bouykourne <obouykou@e2r7p5.1337.ma>
2020-12-23 13:01:23 +0100

    start

---
+++ micro_paint/mcp.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mcp.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 10:14:46 by obouykou          #+#    #+#             */
+/*   Updated: 2020/12/23 13:01:08 by obouykou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mp.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	if (!s)
+		return ;
+	while (*s)
+		ft_putchar(*s++);
+}
+
+int		corrupted(int err)
+{
+	if (err == 1)
+		ft_putstr("Error: Operation file corrupted\n");
+	else
+		ft_putstr("Error: Operation file is not valid\n");
+	return (1);
+}
+
+int		check_args_file(int ac, char **av, FILE **f, t_mp *mp)
+{
+	if (ac != 2)
+	{
+		ft_putstr("Error: argument\n");
+		return (1);
+	}
+	if (!(*f = fopen(av[1], "r+")) || fscanf(*f, "%d %d %c", &mp->bg.w, &mp->bg.h, &mp->bg.c) != 3)
+		return (corrupted(1));
+	return (0);
+}
+
+char **	alloc_fill_arr(int w, int h, int b_char)
+{
+	int i;
+	char **matrix;
+
+	matrix = (char **)malloc((h + 1) * sizeof(char *));
+	matrix[h] = NULL;
+	i = -1;
+	while (++i < h)
+	{
+		matrix[i] = (char *)malloc(w + 1);
+		memset(matrix[i], b_char, w);
+		matrix[i][w] = '\0';
+	}
+	return (matrix);
+}
+
+
+
+int		main(int ac, char **av)
+{
+	FILE *f;
+	t_mp *mp;
+	int i;
+	int j;
+	int b;
+	
+	if (!(mp = (t_mp *)malloc(sizeof(t_mp))))
+		return (1);
+	if (check_args_file(ac, av, &f, mp))
+		return (1);
+	if (!is_vld(mp->bg.w, mp->bg.h) || !mp->bg.c)
+		return (corrupted(2));
+	mp->arr = alloc_fill_arr(mp->bg.w, mp->bg.h, mp->bg.c);
+	b = 5;
+	while ((b = fscanf(f, "\n%c %f %f %f %f %c"), mp->spec, mp->x, mp->y, mp->w, mp->h, mp->c) != EOF)
+	{
+		if (b != 5)
+			return (corrupted(2));
+		if (check_op();
+		
+		
+	}
+	fclose(f);
+	return (0);
+}
---
+++ micro_paint/mp.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mp.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 12:56:12 by obouykou          #+#    #+#             */
+/*   Updated: 2020/12/23 12:59:36 by obouykou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _MP_H_
+# define _MP_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+# define is_vld(w, h) (h > 0 && h <= 300 && w > 0 && w <= 300)
+
+typedef struct	s_indexer
+{
+	int i;
+	int j;
+}				t_indexer;
+
+typedef struct	s_bgd
+{
+	int		w;
+	int		h;
+	char	c;
+}				t_bgd;
+
+typedef	struct	s_mp
+{
+	t_bgd	bg;
+	char	spec;
+	float	x;
+	float	y;
+	float	w;
+	float	h;
+	char	c;
+	char	**rec;
+	char	**arr;
+	t_indexer idx;
+}				t_mp;
+
+#endif
